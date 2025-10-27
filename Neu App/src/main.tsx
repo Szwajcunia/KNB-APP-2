7160ad712b3f28739a776bb1960c78b1ab0a50f1
Szwajcunia <withe.jakub@gmail.com>
2025-10-27 20:47:42 +0100

    Update main.tsx

--- Neu App/src/main.tsx
+++ Neu App/src/main.tsx
@@ -1,28 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './style.css'
-import App from './App'
-import Dashboard from './pages/Dashboard'
-import Planning from './pages/Planning'
-import Picking from './pages/Picking'
-import Admin from './pages/Admin'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    children: [
-      { index: true, element: <Dashboard/> },
-      { path: 'planning', element: <Planning/> },
-      { path: 'picking', element: <Picking/> },
-      { path: 'admin', element: <Admin/> }
-    ]
-  }
-])
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
-)
+);
